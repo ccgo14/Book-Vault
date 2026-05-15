6f312ab033d3bedd87a3c952e638eeb35c9817fd
adriankamunyu <adrianthegreatone7@gmail.com>
2026-05-15 12:36:52 +0300

    commit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,37 +1,3 @@
-
-import { useState } from 'react';
-
-export default function SearchBar({ onSearch, loading }) {
-  const [query, setQuery] = useState('');
-  const [error, setError] = useState('');
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (!query.trim()) {
-        setError("Please enter a book to be searched");
-        return;
-    }
-    setError("")
-    onSearch(query.trim());
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Search"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        className = 'border rounded-xl p-0.5 m-1'
-      />
-      <button type="submit" 
-      disabled={loading || !query.trim()} 
-      className='border rounded-xl p-0.5 m-1 cursor-pointer'>
-        {loading ? 'Searching…' : 'Search'}
-      </button>
-    </form>
-  );
-=======
 import { useState } from 'react';
 
 export default function SearchBar({ onSearch, loading }) {
@@ -68,5 +34,4 @@ export default function SearchBar({ onSearch, loading }) {
       {error && <p style={{ color: 'red', fontSize: '12px' }}>{error}</p>}
     </form>
   );
->>>>>>> origin/dev:src/components/SearchBar.jsx
 }
